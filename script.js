db41f6e49cfec6cbf6d47c16211b845e289cae40
brennaveir <brennaveir@hotmail.com>
2022-12-08 11:28:59 -0500

    added a for loop to loop through options

--- script.js
+++ script.js
@@ -10,39 +10,48 @@ var randomPassword = ""
  if (desiredLength < 8 || desiredLength > 128) {
   alert("invalid input")
  }  
- else {
+else {
+var includeUpper = window.confirm("Include uppercase letters?")
+var includeLower = window.confirm("Include lowercase letters?")
+var includeSymbol = window.confirm("Include symbols?")
+var includeNumber = window.confirm("Include lowercase letters?")
+
   //start for loop to loop through options?
  for (var i = 0; i < desiredLength; i++) {
- 
+
+  
 //choose whether or not to include uppercase
-var includeUpper = window.confirm("Include uppercase letters?")
+
 if (includeUpper) {
 includeUpper = Math.floor((Math.random() * 26) + 65);
 //convert to letter in ascii table
 includeUpper = String.fromCharCode(includeUpper);
-return includeUpper;
+randomPassword = randomPassword.concat(includeUpper)
 }
 
 
 //choose whether or not to include lowercase
-var includeLower = window.confirm("Include lowercase letters?")
+
 if (includeLower) {
 includeLower = Math.floor((Math.random() * 26) + 97);
 includeLower = String.fromCharCode(includeLower);
+randomPassword = randomPassword.concat(includeLower)
 }
 
 //choose whether or not to include special characters
-var includeSymbol = window.confirm("Include symbols?")
+
 if (includeSymbol) {
 includeSymbol = Math.floor((Math.random() * 15) + 33);
 var includeSymbol = String.fromCharCode(includeSymbol);
+randomPassword = randomPassword.concat(includeSymbol)
 }
 
 //choose whether or not to include numbers
-var includeNumber = window.confirm("Include lowercase letters?")
+
 if (includeNumber) {
 includeNumber = Math.floor((Math.random() * 10) + 48);
 var includeNumber = String.fromCharCode(includeNumber);
+randomPassword = randomPassword.concat(includeNumber)
   }
 
 //at least one character type must be chosen
